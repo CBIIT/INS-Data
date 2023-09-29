5a081c08fa3e0b1cc1095fd68adfbef18f9ed430
Zachary Dorman <zachary.dorman@nih.gov>
2023-09-29 16:14:28 -0400

    Disable optional version override in config

--- config.py
+++ config.py
@@ -27,10 +27,10 @@ CLEANED_KEY_PROGRAMS_CSV = "data/cleaned/key_programs_" + QUALTRICS_VERSION + ".
 TIMESTAMP = 'api-gathered-'+datetime.now().strftime('%Y-%m-%d')
 
 # # OPTIONAL MANUAL OVERRIDE for debugging and running modules on older data
-OVERRIDE_DATE = '2023-09-28' # <-- INPUT override date of API gathering version
-TIMESTAMP = 'api-gathered-' + OVERRIDE_DATE
-print(f"---TIMESTAMP OVERRIDE IN USE---\n"
-      f"---Disable this with comments in config.py for default behavior---")
+# OVERRIDE_DATE = '2023-09-28' # <-- INPUT override date of API gathering version
+# TIMESTAMP = 'api-gathered-' + OVERRIDE_DATE
+# print(f"---TIMESTAMP OVERRIDE IN USE---\n"
+#       f"---Disable this with comments in config.py for default behavior---")
 
 # Versioned directories for intermediates and outputs
 PROCESSED_DIR = "data/processed/" + QUALTRICS_VERSION + "/" + TIMESTAMP
