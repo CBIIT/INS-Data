c22cb023f526bc7c67de38f76d765797179ebb1d
Zack Dorman <82673924+zdorman@users.noreply.github.com>
2024-03-18 13:49:14 -0400

    Add result counts for program search
    - Add function `get_downstream_node_records` to pull records
    associated with ids from upstream values. For instance, "get all
    projects associated with programs with 'ccdi' program id"
    - Add function `get_single_program_output_counts` to iterate
    through nodes to pull counts of projects, grants, and publications
    associated with each program

--- modules/build_validation_files.py
+++ modules/build_validation_files.py
@@ -5,6 +5,9 @@ build_validation_files.py
 This module defines primary function build_validation_files that generates files
 useful for summarizing the data gathered in this pipeline. The files are intended
 to act as the expected source of truth when QA testing INS data functionality.
+
+Data output TSVs will not be changed by this module. It will only read them in 
+order to build validation files. 
 """
 
 import os
@@ -85,6 +88,121 @@ def get_all_node_counts(df_list: pd.DataFrame) -> pd.DataFrame:
                                    ignore_index=True)
 
     return df_node_counts
+
+
+
+def get_downstream_node_records(df_downstream: pd.DataFrame,
+                                link_id: str,
+                                link_values: str | list):
+    """
+    Generic function to pull all associated records from a single downstream 
+    node with link values. Handles either string or list for link_values.
+    
+    Args:
+        df_downstream (pd.DataFrame): Dataframe containing a link_id that can
+            associate records with the upstream Dataframes.
+        link_id (str): String name of column containing linking ids
+        link_values (list | str): ID or list of IDs to use for filteirng records
+
+    Returns: 
+        linked_records (pd.Dataframe): Dataframe of all downstream records with
+            matching linking values. 
+
+    Example: 
+        To records for all projects associated with a program, use args:
+            df_downstream = df_projects,
+            link_id = 'program.program_id' # Column within df_projects
+            link_values = 'ccdi' # Program ID within program.program_id column
+    """
+
+    # If a single link_value is provided:
+    if isinstance(link_values, str):  
+
+        # Filter for exact string match
+        linked_records = df_downstream[df_downstream[link_id] == link_values]
+
+    # If a list of linking values is provided:
+    else:
+
+        # Filter for all records containing any link values
+        linked_records = df_downstream[df_downstream[link_id].isin(link_values)]
+
+    return linked_records
+
+
+
+
+def build_single_program_output_counts(df_programs: pd.DataFrame,
+                                        df_projects: pd.DataFrame,
+                                        df_grants: pd.DataFrame,
+                                        df_publications: pd.DataFrame):
+    """
+    Generates a dataframe summarizing counts of outputs (projects, grants,
+        and publications) for each program. 
+
+    Args:
+        df_programs (pd.DataFrame): Dataframe from program.tsv
+        df_projects (pd.DataFrame): Dataframe from project.tsv
+        df_grants (pd.DataFrame): Dataframe from grant.tsv
+        df_publications (pd.DataFrame): Dataframe from publication.tsv
+    """
+    
+    # Build empty dataframe to fill with results
+    df_program_results = pd.DataFrame()
+
+    # Get list of all unique program ids
+    program_id_list = df_programs['program_id'].unique().tolist()
+
+    # Iterate through each program
+    for program_id in program_id_list:
+
+        # Get single program record from programs df
+        df_program_filtered = (df_programs[
+                                df_programs['program_id'] == program_id]
+                                ).reset_index()
+
+        # Get program name for output
+        program_name = df_program_filtered['program_name']
+        
+        # Get program acronym for output
+        program_acronym = df_program_filtered['program_acronym']
+
+        # Get projects linked to program
+        linked_projects = get_downstream_node_records(df_projects, 
+                                                      'program.program_id', 
+                                                       program_id)
+        project_id_list = linked_projects['project_id'].unique() 
+
+        # Get grants linked to project (indirectly to program)
+        linked_grants = get_downstream_node_records(df_grants,
+                                                    'project.project_id',
+                                                     project_id_list)
+        grant_id_list = linked_grants['grant_id'].unique()
+
+        # Get publications linked to project (indirectly to program)
+        linked_publications = get_downstream_node_records(df_publications, 
+                                                          'project.project_id', 
+                                                           project_id_list)
+        publication_id_list = linked_publications['pmid'].unique()
+
+        # Create a dictionary with results
+        program_result_row = {
+            'program_id': program_id,
+            'program_name': program_name,
+            'program_acronym': program_acronym,
+            'projects': len(project_id_list),
+            'grants': len(grant_id_list),
+            'publications': len(publication_id_list)
+            }
+        
+        # Convert to df
+        result_row_df = pd.DataFrame(program_result_row, index=[0])
+
+        # Add to running list of full results
+        df_program_results = pd.concat([df_program_results, result_row_df], 
+                                       ignore_index=True)
+    
+    return df_program_results
 
 
 
