38f7a8fd0516512d86fd829fa33098bbf79abaeb
Zack Dorman <82673924+zdorman@users.noreply.github.com>
2026-03-02 13:32:03 -0500

    INS-1554: Keep curated CTD2 as-is during packaging
    - Rename CTD2 curated output file to remove empty space
    - Update config with curated filepath

--- modules/package_output_data.py
+++ modules/package_output_data.py
@@ -668,24 +668,29 @@ def package_cedcd_datasets(df_cedcd_datasets, column_configs):
 
 
 
-def package_ctd2_datasets(df_ctd2_datasets, column_configs):
+def package_ctd2_datasets(df_ctd2_datasets, column_configs, ctd2_curated=False):
     """Package CTD^2 Datasets data for INS loading."""
 
     print(f"---\nFinalizing TSV for CTD^2 Datasets data...") 
 
-    # Standardize and validate data
-    df_ctd2_datasets_output = standardize_data(df_ctd2_datasets, column_configs, 
-                                        datatype='ctd2_dataset')
+    if ctd2_curated:
+        print(f"Locked curation of CTD^2 datasets found. Keep without changing or resaving.")
+        return df_ctd2_datasets
 
-    # Export as TSV
-    output_filepath = config.CTD2_DATASET_OUTPUT_PATH
-    os.makedirs(os.path.dirname(output_filepath), exist_ok=True)
-    df_ctd2_datasets_output.to_csv(output_filepath, sep='\t', index=False, 
-                                    encoding='utf-8')
+    else: 
+        # Standardize and validate data
+        df_ctd2_datasets_output = standardize_data(df_ctd2_datasets, column_configs, 
+                                            datatype='ctd2_dataset')
 
-    print(f"Done! Final CTD^2 Datasets data saved as {output_filepath}.")
+        # Export as TSV
+        output_filepath = config.CTD2_DATASET_OUTPUT_PATH
+        os.makedirs(os.path.dirname(output_filepath), exist_ok=True)
+        df_ctd2_datasets_output.to_csv(output_filepath, sep='\t', index=False, 
+                                        encoding='utf-8')
 
-    return df_ctd2_datasets_output
+        print(f"Done! Final CTD^2 Datasets data saved as {output_filepath}.")
+
+        return df_ctd2_datasets_output
 
 
 def package_ctd2_filedata(df_ctd2_filedata, column_configs):
@@ -988,8 +993,14 @@ def package_output_data():
         print(f"No CEDCD Datasets file found.")
 
     # Load CTD2 datasets data
-    if os.path.exists(config.CTD2_DATASET_INTERMED_CSV):
+    if os.path.exists(config.CTD2_DATASET_CURATED_LOCKED_PATH):
         ctd2_datasets_exist = True
+        ctd2_curated = True
+        df_ctd2_datasets = pd.read_csv(config.CTD2_DATASET_CURATED_LOCKED_PATH, sep='\t')
+        print(f"Loaded CTD2 Datasets (Locked Curation) file from {config.CTD2_DATASET_CURATED_LOCKED_PATH}")
+    elif os.path.exists(config.CTD2_DATASET_INTERMED_CSV):
+        ctd2_datasets_exist = True
+        ctd2_curated = False
         df_ctd2_datasets = pd.read_csv(config.CTD2_DATASET_INTERMED_CSV)
         print(f"Loaded CTD2 Datasets file from {config.CTD2_DATASET_INTERMED_CSV}")
     else:
@@ -1050,7 +1061,8 @@ def package_output_data():
     if cedcd_datasets_exist:
         df_cedcd_datasets_out = package_cedcd_datasets(df_cedcd_datasets, column_configs)
     if ctd2_datasets_exist:
-        df_ctd2_datasets_out = package_ctd2_datasets(df_ctd2_datasets, column_configs)
+        df_ctd2_datasets_out = package_ctd2_datasets(df_ctd2_datasets, column_configs,
+                                                     ctd2_curated)
     if ctd2_filedata_exist:
         df_ctd2_filedata_out = package_ctd2_filedata(df_ctd2_filedata, column_configs)
 
